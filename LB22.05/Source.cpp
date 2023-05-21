5f70c8d4befe7da96397e504faad65932a70a770
Геннадий <@DESKTOP-6NN51IE>
2023-05-21 12:23:40 +0300

    ex_done

---
+++ LB22.05/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Kraj lukomor’a dub zelenyj," << std::endl;
+    std::cout << "I zolotyj lancyuh na nim:" << std::endl;
+    std::cout << "Shodnya, shonochi kit uchenyj" << std::endl;
+    std::cout << "Na lancyuhu kruzhl’aje tim" << std::endl;
+
+    // Ñóïğîâ³äíèé çâóêîâèé ñèãíàë
+    std::cout << "\a\a\a";
+
+    return 0;
+}
